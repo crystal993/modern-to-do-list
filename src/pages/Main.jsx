87bee4531c6d65f957b54639b9cd274fe1efb434
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-10-11 18:29:32 +0900

    <feat> 메인 페이지에 GlobalToggle 적용

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import Toggle from "../components/elements/GlobalToggle";
 import SignIn from "../components/signin/SignIn";
 import SignUp from "../components/signup/SignUp";
 const Main = () => {
+  const [isToggled, setIsToggled] = useState(false);
+
+  const onToggleHandler = () => {
+    if (!isToggled) {
+      setIsToggled(true);
+    } else {
+      setIsToggled(false);
+    }
+  };
   return (
     <Wrapper>
-      <SignIn />
-      <SignUp />
+      <ToggleWrapper>
+        Login
+        <Toggle isToggled={isToggled} onToggle={onToggleHandler} />
+        SignUp
+      </ToggleWrapper>
+      <FormWrapper>{!isToggled ? <SignIn /> : <SignUp />}</FormWrapper>
     </Wrapper>
   );
 };
@@ -16,5 +30,26 @@ const Wrapper = styled.div`
   width: 100%;
   min-height: 100vh;
   background-color: ${({ theme }) => theme.mainColor};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ToggleWrapper = styled.div`
+  width: 40%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 0.4rem 0rem;
+  border-radius: 0.5rem;
+  gap: 0.5rem;
+  font-size: 0.8rem;
+  color: ${({ theme }) => theme.backgroundColor};
+`;
+
+const FormWrapper = styled.div`
+  width: 100%;
 `;
 export default Main;
