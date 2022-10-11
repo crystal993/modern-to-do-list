3808f58ce20c8d43f8721ee6f166fd0809012155
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-10-11 21:42:02 +0900

    <feat> 로그인 페이지 폼 유효성 검사

--- src/components/signin/SignIn.jsx
+++ src/components/signin/SignIn.jsx
@@ -3,25 +3,59 @@ import styled from "styled-components";
 import Button from "../elements/GlobalButton";
 import Input from "../elements/GlobalInput";
 import Label from "../elements/GlobalLabel";
+import { emailCheck, passwordCheck } from "../../shared/regex";
 
-const SignIn = () => {
+const SignIn = ({ setIsToggled }) => {
   const initialState = {
     email: "",
     password: "",
   };
 
   const [formData, setFormData] = useState(initialState);
+  const [emailMsg, setEmailMsg] = useState("");
+  const [pwMsg, setPwMsg] = useState("");
+  const [emailMsgColor, setEmailMsgColor] = useState("");
+  const [pwMsgColor, setPwMsgColor] = useState("");
 
-  const onChangeHandler = (e) => {};
-  const onSubmitHandler = () => {};
+  const onChangeHandler = (e) => {
+    const { value, name } = e.target;
+    setFormData({ ...formData, [name]: value });
+    if (name === "email") {
+      if (!emailCheck(value)) {
+        setEmailMsg("@를 포함해주세요.");
+        setEmailMsgColor("#FF1F2F");
+      } else {
+        setEmailMsg("올바른 형식입니다.");
+        setEmailMsgColor("#5D5FEF");
+      }
+    }
+
+    if (name === "password") {
+      if (passwordCheck(value)) {
+        setPwMsg("올바른 형식입니다.");
+        setPwMsgColor("#5D5FEF");
+      } else {
+        setPwMsg("8자리 이상 입력해주세요.");
+        setPwMsgColor("#FF1F2F");
+      }
+    }
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    if (!emailCheck(formData.email) || !passwordCheck(formData.password)) {
+      alert("아이디나 비밀번호 형식이 틀렸습니다.");
+    } else {
+      setIsToggled(false);
+    }
+  };
   return (
     <Wrapper onSubmit={onSubmitHandler}>
-      <Title>LOGIN</Title>
       <Title>로그인</Title>
       <Form>
         <InputWrapper>
           <Label
-            label={"Email"}
             label={"이메일"}
             labelFontSize={"1rem"}
             width={"100%"}
@@ -31,12 +65,13 @@ const SignIn = () => {
             type={"text"}
             name={"email"}
             value={formData.email}
-            onChangeHandler={onChangeHandler}
+            onChange={onChangeHandler}
             required={"required"}
             placeholder={"이메일을 입력해주세요."}
             width={"100%"}
             fontSize={"1rem"}
           />
+          <Label label={emailMsg} color={emailMsgColor}></Label>
         </InputWrapper>
         <InputWrapper>
           <Label
@@ -49,12 +84,13 @@ const SignIn = () => {
             type={"password"}
             name={"password"}
             value={formData.password}
-            onChangeHandler={onChangeHandler}
+            onChange={onChangeHandler}
             required={"required"}
             placeholder={"8자리 이상 입력해주세요."}
             width={"100%"}
             fontSize={"1rem"}
           />
+          <Label label={pwMsg} color={pwMsgColor}></Label>
         </InputWrapper>
         <ButtonWrapper>
           <Button
