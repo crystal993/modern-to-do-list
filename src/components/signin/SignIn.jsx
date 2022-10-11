3f2e81f5bce1103d42845e57a66f515d540b71df
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-10-11 17:55:05 +0900

    <feat> SignIn 컴포넌트 css

---
+++ src/components/signin/SignIn.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Button from "../elements/GlobalButton";
+import Input from "../elements/GlobalInput";
+import Label from "../elements/GlobalLabel";
+
+const SignIn = () => {
+  const initialState = {
+    email: "",
+    password: "",
+  };
+
+  const [formData, setFormData] = useState(initialState);
+
+  const onChangeHandler = (e) => {};
+  const onSubmitHandler = () => {};
+  return (
+    <Wrapper onSubmit={onSubmitHandler}>
+      <Title>LOGIN</Title>
+      <Form>
+        <InputWrapper>
+          <Label
+            label={"Email"}
+            labelFontSize={"1rem"}
+            width={"100%"}
+            htmlFor={"email"}
+          />
+          <Input
+            type={"text"}
+            name={"email"}
+            value={formData.email}
+            onChangeHandler={onChangeHandler}
+            placeholder={"이메일을 입력해주세요."}
+            width={"100%"}
+            fontSize={"1rem"}
+          />
+        </InputWrapper>
+        <InputWrapper>
+          <Label
+            label={"Password"}
+            labelFontSize={"1rem"}
+            width={"100%"}
+            htmlFor={"password"}
+          />
+          <Input
+            type={"password"}
+            name={"password"}
+            value={formData.password}
+            onChangeHandler={onChangeHandler}
+            placeholder={"8자리 이상 입력해주세요."}
+            width={"100%"}
+            fontSize={"1rem"}
+          />
+        </InputWrapper>
+        <ButtonWrapper>
+          <Button
+            width={"100%"}
+            height={"1rem"}
+            content={"로그인"}
+            fontSize={"0.8rem"}
+          />
+        </ButtonWrapper>
+      </Form>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 40%;
+  border-radius: 10px;
+  background-color: #ffffff;
+  margin: 0rem auto;
+  display: flex;
+  flex-direction: column;
+  align-content: space-around;
+  justify-content: center;
+  flex-wrap: nowrap;
+  align-items: center;
+  padding: 5rem 0rem;
+`;
+
+const Title = styled.h1`
+  color: ${({ theme }) => theme.subColor};
+  font-size: 1.8rem;
+  font-weight: bold;
+  margin-bottom: 0rem;
+`;
+
+const Form = styled.form`
+  width: 100%;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const InputWrapper = styled.div`
+  margin: 1rem 0rem;
+`;
+
+const ButtonWrapper = styled.div`
+  margin-top: 1rem;
+`;
+
+export default SignIn;
