b5340a3562c7cc2d1bf079f435213b34a5760c5a
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-10-12 18:43:00 +0900

    <feat> accessToken이 없을 때 로그인페이지로 리다이렉트

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 import CreateForm from "../components/todo/CreateForm";
 import TodoList from "../components/todo/TodoList";
 
 const Todo = () => {
+  const navigate = useNavigate();
   const [todoList, setTodoList] = useState([]);
+  const accessToken = localStorage.getItem("access_token");
+  useEffect(() => {
+    if (!accessToken) {
+      navigate("/");
+    }
+  }, [accessToken, navigate]);
 
   return (
     <Wrapper>
