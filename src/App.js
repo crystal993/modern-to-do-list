16096a2bc49ee73472af96be464408462806335c
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-10-12 18:18:32 +0900

    <feat> GlobalStyle에 styled-reset 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Router from "./router/Router";
 import GlobalStyle from "./styles/GlobalStyle";
+import { Reset } from "styled-reset";
 function App(props) {
   return (
     <>
+      <Reset />
       <GlobalStyle />
       <Router />
     </>
--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -1,64 +1,22 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
-// reset all html tags with createGlobalStyle
 const GlobalStyle = createGlobalStyle`
-// CSS 리셋
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, menu, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-main, menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, main, menu, nav, section {
-  display: block;
-}
-/* HTML5 hidden-attribute fix for newer browsers */
-*[hidden] {
-    display: none;
-}
-body {
-  line-height: 1;
-}
-menu, ol, ul {
-  list-style: none;
-}
-blockquote, q {
-  quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-  content: '';
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
-// add global styles : 시용자 정의 글로벌 스타일 추가
+  ${reset}
 * {
 	box-sizing: border-box;
 }
 body {
   font-size: 62.5%;
 	background-color: #5D5FEF;
+  font-family: 'Nanum Gothic Coding', monospace;
 }
 body * {
 	background-color: transparent;
 	letter-spacing: -0.5px;
+}
+h1 {
+  font-family: 'Roboto', sans-serif;
 }
 a {
 	text-decoration: none;
